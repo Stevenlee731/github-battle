c77fdced93c6c1a65c625139dc241c5fa3592947
stevecreated@gmail.com <stevecreated@gmail.com>
2017-05-29 23:39:16 -0700

    axios

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,6 +2,36 @@ const React = require('react')
 const PropTypes = require('prop-types')
 const api = require('../utils/api')
 
+const RepoGrid = props => {
+  return (
+    <ul className='popular-list'>
+      {props.repos.map((repo, index) => {
+        return (
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'>#{index + 1}</div>
+            <ul className='space-list-items'>
+              <li>
+                <img
+                  className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={'Avatar for ' + repo.owner.login}
+                />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+}
+
 const SelectLanguage = props => {
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
   return (
@@ -61,6 +91,9 @@ class Popular extends React.Component {
         <SelectLanguage
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage} />
+          {!this.state.repos
+            ? <p>Loading</p>
+            : <RepoGrid repos={this.state.repos}/>}
       </div>
     )
   }
